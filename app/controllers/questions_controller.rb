e4c0e5442fffff08783e4f42afb46c2679d982d8
Gabriel A <ariinuitahiti@gmail.com>
2024-11-18 12:03:03 -0500

    poor coach logic implemented

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,20 @@
 class QuestionsController < ApplicationController
+
+  @coach_answer = ''
+
+  def ask
+  end
+
+  def answer
+    @answer = params[:question]
+    case @answer
+    when "I am going to work"
+      @coach_answer = "Great!"
+    when @answer[-1] == '?'
+      @coach_answer = "Silly question, get dressed and go to work!"
+    else
+      @coach_answer = "I don't care, get dressed and go to work!"
+    end
+  end
+
 end
